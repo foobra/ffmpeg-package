10ca5112739725758881edb1507189177fa468d0
gs <gaoshan_yy@migu.cn>
2020-04-03 00:19:58 +0800

    replace with tar

--- lame-3.100/compile.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-source ~/.bashrc
-source /opt/rh/devtoolset-9/enable
-./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin" --with-pic --disable-shared --enable-nasm  && make -j8 && make install
