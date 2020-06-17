d62c70d2b06e7f4bcfe233fb79b2d1186f3c8ec2
gs <gaoshan_yy@migu.cn>
2020-06-17 21:01:50 +0800

    compile mp3lame with fpic

--- compile_mp3lame.sh
+++ compile_mp3lame.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 cd packages/lame-3.100
 if [ -f /etc/redhat-release ]; then
-    ./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin" --disable-shared --enable-nasm && make -j8 && make install
+    ./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin" --disable-shared --enable-nasm --with-pic  && make -j8 && make install
 fi
 
 if [ -f /etc/lsb-release ]; then
-	PATH="$HOME/bin:$PATH" ./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin" --disable-shared --enable-nasm && \
+	PATH="$HOME/bin:$PATH" ./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin" --disable-shared --enable-nasm --with-pic && \
 PATH="$HOME/bin:$PATH" make -j `nproc` && make install
 fi
 cd -
