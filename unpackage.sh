2c27a78e38f21cc2d0c7bd94c7263e3e47fd7f60
gs <gaoshan_yy@migu.cn>
2020-06-17 16:03:39 +0800

    add unpackage.sh

---
+++ unpackage.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+rm -rf packages || true
+mkdir packages  || true
+
+tar -xvf ffmpeg-4.2.2.tar.gz -C packages
+tar -xvf yasm-1.3.0.tar.gz -C packages
+tar -xvf nasm-2.14.02.tar.bz2 -C packages
+tar -xvf x264.tar -C packages
