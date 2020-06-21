89cc90e4e4d40194b5abb6fe431d16eb50da4bea
gs <gaoshan_yy@migu.cn>
2020-06-21 15:13:12 +0800

    add id3lib

--- compile.sh
+++ compile.sh
@@ -9,3 +9,5 @@ sh compile_x264.sh
 sh compile_mp3lame.sh
 
 sh compile_ffmpeg.sh
+
+sh compile_id3.sh
--- unpackage.sh
+++ unpackage.sh
@@ -8,3 +8,4 @@ tar -xvf yasm-1.3.0.tar.gz -C packages
 tar -xvf nasm-2.14.02.tar.bz2 -C packages
 tar -xvf x264.tar -C packages
 tar -xvf lame-3.100.tar.gz -C packages
+tar -xvf id3lib-3.8.3.tar.gz -C packages
