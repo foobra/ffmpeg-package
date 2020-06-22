04190793ba1a0c3b6757c1e9741c2ff9b24e8daf
gs <gaoshan_yy@migu.cn>
2020-06-22 18:31:25 +0800

    compile id3 with pic

--- compile_id3.sh
+++ compile_id3.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 cd packages/id3lib-3.8.3
 if [ -f /etc/redhat-release ]; then
-    ./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin" && make -j8 && make install
+    ./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin" --with-pic && make -j8 && make install
 fi
 
 if [ -f /etc/lsb-release ]; then
-	PATH="$HOME/bin:$PATH" ./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin" && \
+	PATH="$HOME/bin:$PATH" ./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin" --with-pic && \
 PATH="$HOME/bin:$PATH" make -j `nproc` && make install
 fi
 cd -
